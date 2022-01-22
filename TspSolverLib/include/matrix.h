ab4625365610fdd88a1f39b72d75d2752239d146
Doringus <skomoroshchenkodenis@gmail.com>
2022-01-22 15:48:36 +0600

    Removed old vector of vectors matrix

--- TspSolverLib/include/matrix.h
+++ TspSolverLib/include/matrix.h
@@ -6,130 +6,308 @@
 #include <cassert>
 #include <vector>
 #include <functional>
+#include <type_traits>
 
 namespace tspsolver {
 
-    template <typename T>
-    concept Number = std::integral<T> || std::floating_point<T>;
-
-    template <typename Type> requires Number<Type>
+    template <typename Type, std::enable_if_t<std::is_arithmetic_v<Type>, bool> = true>
     class SquareMatrix {
     public:
-        using RowIterator = typename std::vector<Type>::iterator;
-        using RowCIterator = typename std::vector<Type>::const_iterator;
-        using ColumnIterator = typename std::vector<std::reference_wrapper<Type>>::iterator;
-        using ColumnCIterator = typename std::vector<std::reference_wrapper<Type>>::const_iterator;
-        using RowsIterator = typename  std::vector<std::vector<Type>>::iterator;
-        using RowsCIterator = typename  std::vector<std::vector<Type>>::const_iterator;
-        using ColumnsIterator = typename std::vector<std::vector<std::reference_wrapper<Type>>>::iterator;
-        using ColumnsCIterator = typename std::vector<std::vector<std::reference_wrapper<Type>>>::const_iterator;
 
-        explicit SquareMatrix(std::size_t size) {
-            m_Data.resize(size);
-            m_Columns.resize(size);
-            for(std::size_t i = 0; i < size; ++i) {
-                m_Data[i].resize(size);
-                for(std::size_t j = 0; j < size; ++j) {
-                    m_Data[i][j] = 0;
-                    m_Columns[j].push_back(m_Data[i][j]);
-                }
+        class RowConstIterator {
+        public:
+            using iterator_category = std::random_access_iterator_tag;
+            using value_type        = const Type;
+            using difference_type   = std::ptrdiff_t;
+            using pointer           = const Type*;
+            using reference         = const Type&;
+
+            explicit RowConstIterator(Type* begin) : m_Ptr(begin) {
+
             }
-        }
 
-        SquareMatrix(const SquareMatrix& matrix) : m_Data(matrix.m_Data) {
-            std::size_t size = m_Data.size();
-            m_Columns.resize(size);
-            for(std::size_t i = 0; i < size; ++i) {
-                for(std::size_t j = 0; j < size; ++j) {
-                    m_Columns[j].push_back(m_Data[i][j]);
-                }
+            constexpr reference operator*() const {
+                return *m_Ptr;
             }
+
+            constexpr pointer operator->() const {
+                return m_Ptr;
+            }
+
+            constexpr RowConstIterator& operator++() {
+                ++m_Ptr;
+                return *this;
+            }
+
+            constexpr RowConstIterator operator++(int) {
+                RowConstIterator tmp = *this;
+                ++m_Ptr;
+                return tmp;
+            }
+
+            constexpr RowConstIterator& operator--() {
+                m_Ptr--;
+                return *this;
+            }
+
+            constexpr RowConstIterator operator--(int) {
+                RowConstIterator tmp = *this;
+                --m_Ptr;
+                return tmp;
+            }
+
+            friend constexpr bool operator==(const RowConstIterator& lhs, const RowConstIterator& rhs) {
+                return lhs.m_Ptr == rhs.m_Ptr;
+            }
+            friend constexpr bool operator!=(const RowConstIterator& lhs, const RowConstIterator& rhs) {
+                return lhs.m_Ptr != rhs.m_Ptr;
+            }
+            friend constexpr size_t operator-(const RowConstIterator& lhs, const RowConstIterator& rhs) {
+                return lhs.m_Ptr - rhs.m_Ptr;
+            }
+
+            Type* m_Ptr;
+        };
+
+        class RowIterator : public RowConstIterator {
+        public:
+            using iterator_category = std::random_access_iterator_tag;
+            using value_type        = Type;
+            using difference_type   = std::ptrdiff_t;
+            using pointer           = Type*;
+            using reference         = Type&;
+
+            explicit RowIterator(pointer begin) : RowConstIterator(begin) {
+
+            }
+
+            constexpr reference operator*() const {
+                return const_cast<reference>(RowConstIterator::operator*());
+            }
+
+            constexpr pointer operator->() const {
+                return this->m_Ptr;
+            }
+
+            constexpr RowIterator& operator++() {
+                RowConstIterator::operator++();
+                return *this;
+            }
+
+            constexpr RowIterator operator++(int) {
+                RowIterator tmp = *this;
+                RowConstIterator::operator++();
+                return tmp;
+            }
+
+            constexpr RowIterator& operator--() {
+                RowConstIterator::operator--();
+                return *this;
+            }
+
+            constexpr RowIterator operator--(int) {
+                RowIterator tmp = *this;
+                RowConstIterator::operator--();
+                return tmp;
+            }
+
+            friend constexpr bool operator==(const RowIterator& lhs, const RowIterator& rhs) {
+                return lhs.m_Ptr == rhs.m_Ptr;
+            }
+            friend constexpr bool operator!=(const RowIterator& lhs, const RowIterator& rhs) {
+                return lhs.m_Ptr != rhs.m_Ptr;
+            }
+            friend constexpr size_t operator-(const RowIterator& lhs, const RowIterator& rhs) {
+                return lhs.m_Ptr - rhs.m_Ptr;
+            }
+        };
+
+
+        class ColumnConstIterator {
+        public:
+
+            using iterator_category = std::random_access_iterator_tag;
+            using value_type        = const Type;
+            using difference_type   = std::ptrdiff_t;
+            using pointer           = const Type*;
+            using reference         = const Type&;
+
+            explicit ColumnConstIterator(Type* begin, std::size_t step) : m_Ptr(begin), m_Step(step) {
+
+            }
+
+            constexpr reference operator*() const {
+                return *m_Ptr;
+            }
+
+            constexpr pointer operator->() const {
+                return m_Ptr;
+            }
+
+            constexpr ColumnConstIterator& operator++() {
+                m_Ptr += m_Step;
+                return *this;
+            }
+
+            constexpr ColumnConstIterator operator++(int) {
+                ColumnConstIterator tmp = *this;
+                m_Ptr += m_Step;
+                return tmp;
+            }
+
+            constexpr ColumnConstIterator& operator--() {
+                m_Ptr -= m_Step;
+                return *this;
+            }
+
+            constexpr ColumnConstIterator operator--(int) {
+                ColumnConstIterator tmp = *this;
+                m_Ptr -= m_Step;
+                return tmp;
+            }
+
+            friend constexpr bool operator==(const ColumnConstIterator& lhs, const ColumnConstIterator& rhs) {
+                return lhs.m_Ptr == rhs.m_Ptr;
+            }
+            friend constexpr bool operator!=(const ColumnConstIterator& lhs, const ColumnConstIterator& rhs) {
+                return lhs.m_Ptr != rhs.m_Ptr;
+            }
+            friend constexpr size_t operator-(const ColumnConstIterator& lhs, const ColumnConstIterator& rhs) {
+                return lhs.m_Ptr - rhs.m_Ptr;
+            }
+
+            Type* m_Ptr;
+            std::size_t m_Step;
+        };
+
+        class ColumnIterator : public ColumnConstIterator {
+        public:
+            using iterator_category = std::random_access_iterator_tag;
+            using value_type        = Type;
+            using difference_type   = std::ptrdiff_t;
+            using pointer           = Type*;
+            using reference         = Type&;
+
+            explicit ColumnIterator(pointer begin, std::size_t step) : ColumnConstIterator(begin, step) {
+
+            }
+
+            constexpr reference operator*() const {
+                return const_cast<reference>(ColumnConstIterator::operator*());
+            }
+
+            constexpr pointer operator->() const {
+                return this->m_Ptr;
+            }
+
+            constexpr ColumnIterator& operator++() {
+                ColumnConstIterator::operator++();
+                return *this;
+            }
+
+            constexpr ColumnIterator operator++(int) {
+                ColumnIterator tmp = *this;
+                ColumnConstIterator::operator++();
+                return tmp;
+            }
+
+            constexpr ColumnIterator& operator--() {
+                ColumnConstIterator::operator--();
+                return *this;
+            }
+
+            constexpr ColumnIterator operator--(int) {
+                ColumnIterator tmp = *this;
+                ColumnConstIterator::operator--();
+                return tmp;
+            }
+
+            friend constexpr bool operator==(const ColumnIterator& lhs, const ColumnIterator& rhs) {
+                return lhs.m_Ptr == rhs.m_Ptr;
+            }
+            friend constexpr bool operator!=(const ColumnIterator& lhs, const ColumnIterator& rhs) {
+                return lhs.m_Ptr != rhs.m_Ptr;
+            }
+            friend constexpr size_t operator-(const ColumnIterator& lhs, const ColumnIterator& rhs) {
+                return lhs.m_Ptr - rhs.m_Ptr;
+            }
+        };
+
+    public:
+
+        using ElementsIterator = typename std::vector<Type>::iterator;
+        using ElementsCIterator = typename std::vector<Type>::const_iterator;
+
+
+        explicit constexpr SquareMatrix(std::size_t size) : m_Size(size) {
+            m_Data.resize(size * size);
         }
 
-        SquareMatrix(SquareMatrix&& matrix) noexcept: m_Data(std::exchange(matrix.m_Data, {})),
-                        m_Columns(std::exchange(matrix.m_Columns, {})) {
 
+        virtual constexpr Type& at(std::size_t rowIndex, std::size_t columnIndex) {
+            return m_Data[rowIndex * m_Size + columnIndex];
         }
 
-        constexpr std::size_t getSize() const noexcept {
-            return m_Data.size();
+        virtual constexpr Type const& at(std::size_t rowIndex, std::size_t columnIndex) const {
+            return m_Data.at(rowIndex * m_Size + columnIndex);
         }
 
-        constexpr Type& at(std::size_t rowIndex, std::size_t columnIndex) {
-            return m_Data.at(rowIndex).at(columnIndex);
+        constexpr std::size_t size() const noexcept {
+            return m_Size;
         }
 
-        constexpr Type const& at(std::size_t rowIndex, std::size_t columnIndex) const {
-            return m_Data[rowIndex][columnIndex];
-        }
-
-        constexpr RowIterator rowBegin(std::size_t rowIndex) {
-            return m_Data[rowIndex].begin();
-        }
-
-        constexpr RowCIterator rowBegin(std::size_t rowIndex) const {
-            return m_Data[rowIndex].cbegin();
-        }
-
-        constexpr RowIterator rowEnd(std::size_t rowIndex) {
-            return m_Data[rowIndex].end();
-        }
-
-        constexpr RowCIterator rowEnd(std::size_t rowIndex) const {
-            return m_Data[rowIndex].cend();
-        }
-
-        constexpr ColumnIterator columnBegin(std::size_t columnIndex) {
-            return m_Columns[columnIndex].begin();
-        }
-
-        constexpr ColumnCIterator columnBegin(std::size_t columnIndex) const {
-            return m_Columns[columnIndex].begin();
-        }
-
-        constexpr ColumnIterator columnEnd(std::size_t columnIndex) {
-            return m_Columns[columnIndex].end();
-        }
-
-        constexpr ColumnCIterator columnEnd(std::size_t columnIndex) const {
-            return m_Columns.at(columnIndex).cend();
-        }
-
-        constexpr RowsIterator rowsBegin(){
+        constexpr ElementsIterator begin() noexcept {
             return m_Data.begin();
         }
 
-        constexpr RowsCIterator rowsBegin() const {
-            return m_Data.cbegin();
+        constexpr ElementsCIterator begin() const noexcept {
+            return m_Data.begin();
         }
 
-        constexpr ColumnsIterator columnsBegin() {
-            return m_Columns.begin();
+        constexpr ElementsIterator end() noexcept {
+            return m_Data.end();
         }
 
-        constexpr ColumnsCIterator columnsBegin() const {
-            return m_Columns.cbegin();
+        constexpr ElementsCIterator end() const noexcept {
+            return m_Data.end();
         }
 
-        std::vector<std::vector<Type>>& rows() {
-            return m_Data;
+        constexpr RowConstIterator rowBegin(std::size_t rowIndex) const noexcept {
+            return RowConstIterator(m_Data.data() + rowIndex * m_Size);
         }
 
-        std::vector<std::vector<Type>> const& rows() const {
-            return m_Data;
+        constexpr RowConstIterator rowEnd(std::size_t rowIndex) const noexcept {
+            return RowConstIterator(m_Data.data() + rowIndex * m_Size + m_Size);
         }
 
-        std::vector<std::vector<std::reference_wrapper<Type>>>& columns() {
-            return m_Columns;
+        constexpr RowIterator rowBegin(std::size_t rowIndex) noexcept {
+            return RowIterator(m_Data.data() + rowIndex * m_Size);
         }
 
-        std::vector<std::vector<std::reference_wrapper<Type>>> const& columns() const {
-            return m_Columns;
+        constexpr RowIterator rowEnd(std::size_t rowIndex) noexcept {
+            return RowIterator(m_Data.data() + rowIndex * m_Size + m_Size);
         }
 
-    private:
-        std::vector<std::vector<Type>> m_Data;
-        std::vector<std::vector<std::reference_wrapper<Type>>> m_Columns;
+        constexpr ColumnConstIterator columnBegin(std::size_t columnIndex) const noexcept {
+            return ColumnConstIterator(m_Data.data() + columnIndex, m_Size);
+        }
+
+        constexpr ColumnConstIterator columnEnd(std::size_t columnIndex) const noexcept {
+            return ColumnConstIterator(m_Data.data() + columnIndex + m_Size * m_Size, m_Size);
+        }
+
+        constexpr ColumnIterator columnBegin(std::size_t columnIndex) noexcept {
+            return ColumnIterator(m_Data.data() + columnIndex, m_Size);
+        }
+
+        constexpr ColumnIterator columnEnd(std::size_t columnIndex) noexcept {
+            return ColumnIterator(m_Data.data() + columnIndex + m_Size * m_Size, m_Size);
+        }
+
+
+    protected:
+        std::vector<Type> m_Data;
+        std::size_t m_Size;
     };
 }
 
